f87aa304126baa76e0c42b6a2b7cce0cc8be88cb
recover758126 <761647716@qq.com>
2019-09-11 14:20:48 +0800

    feat: v 1.1.0
    - 迁移到TypeScript
    - 组件样式升级

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,7 +1,37 @@
-import {createElement, Component, render} from 'rax';
+/* eslint-disable import/no-extraneous-dependencies */
+import { createElement, render, useState } from 'rax';
 import Video from '../src/index';
-import * as DriverDOM from 'driver-dom';
-import { isWeex } from 'universal-env';
-import * as DriverWeex from 'driver-weex';
+import View from 'rax-view';
+import Text from 'rax-text';
+import DU from 'driver-universal';
 
-render(<Video style={{ width: 750, height: 400 }} autoPlay controls={false} src="https://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4" />, document.body, { driver: isWeex ? DriverWeex : DriverDOM });
+const App = () => {
+  const [ playStatus, setPlayStatus ] = useState('play');
+  return <View style={{ alignItems: 'center' }}>
+    <Video style={{ width: 750, height: 400 }} playControl={playStatus} autoPlay controls={false} src="https://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4" />
+    <View
+      onClick={() => {
+        if (playStatus === 'play') {
+          setPlayStatus('pause');
+        } else {
+          setPlayStatus('play');
+        }
+      }}
+      style={{
+        width: 300,
+        height: 100,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+      }}
+    >
+      <Text>Switch PlayStatus</Text>
+    </View>
+  </View>;
+};
+
+render(<App />, document.body, { driver: DU });
